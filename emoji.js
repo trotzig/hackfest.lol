6e1bee0cd85ed1948246a2a37840f06ee6857f51
Henric Trotzig <henric.trotzig@gmail.com>
2019-04-13 11:14:14 +0200

    Add google vision api

--- emoji.js
+++ emoji.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
+const path = require('path');
+
+process.env.GOOGLE_APPLICATION_CREDENTIALS = path.resolve(
+  __dirname,
+  'googleCredentials.json',
+);
 
 const crypto = require('crypto');
 const randomFromArray = require('./randomFromArray');
+const vision = require('@google-cloud/vision');
 
 const emojis = {
   smiling: ['🙂', '😊', '😆'],
@@ -16,6 +23,15 @@ function createHash(data) {
 async function run() {
   console.log(randomFromArray(emojis.smiling));
 
-  //const detector = new SmileFaceDetector(); //const image = await detector.load('snapshot.jpg'); //console.log(image);
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+
+  // Performs label detection on the image file
+  const [result] = await client.labelDetection('./snapshot.jpg');
+  const labels = result.labelAnnotations;
+  console.log('Labels:');
+  labels.forEach(label => console.log(label.description));
 }
-run(); // open chrome full screen // /usr/bin/osascript -e "tell application \"Google Chrome\"" -e "activate" -e "make new window" -e "tell application \"System Events\"" -e "keystroke \"f\" using {control down, command down}" -e "end tell" -e "end tell"
+run().catch(e => console.error(e));
+
+// open chrome full screen // /usr/bin/osascript -e "tell application \"Google Chrome\"" -e "activate" -e "make new window" -e "tell application \"System Events\"" -e "keystroke \"f\" using {control down, command down}" -e "end tell" -e "end tell"
