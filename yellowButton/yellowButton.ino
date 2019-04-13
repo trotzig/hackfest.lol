5eb90af33d45ba60dda0c4e6f66e10beddd9e832
Henric Trotzig <henric.trotzig@gmail.com>
2019-04-13 15:49:55 +0200

    Add arduino code

---
+++ yellowButton/yellowButton.ino
@@ -0,0 +1,28 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+
+// digital pin 2 has a pushbutton attached to it. Give it a name:
+int pushButton = 2;
+int buttonState = 0;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(9600);
+  pinMode(pushButton, INPUT);
+ 
+  digitalWrite(pushButton, HIGH);
+  
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input pin:
+  int newButtonState = digitalRead(pushButton);
+  if (newButtonState == 1 && buttonState == 0) {
+    Serial.println("snap");
+  }
+  buttonState = newButtonState;
+  delay(10);        // delay in between reads for stability
+}
